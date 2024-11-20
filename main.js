833aa254829496f8bc7df97f683b86bdd0cdb181
Diyoncrz18 <dkobi16@gmail.com>
2024-11-20 15:21:43 +0800

    update char

--- main.js
+++ main.js
@@ -23,3 +23,33 @@ new Chart(ctx, {
     },
   },
 });
+
+/////////////////////////
+// Chart 2
+
+const chart2 = document.getElementById("myChart2");
+
+new Chart(chart2, {
+  type: "bar",
+  data: {
+    labels: ["USD", "JPY", "AUD", "EUR", "GBP", "CHF"],
+    datasets: [
+      {
+        label: "Rp",
+        data: [15845.35, 102.93, 10302.37, 16715.68, 20055.88, 17930.04],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    backgroundColor: "black",
+    scales: {
+      y: {
+        beginAtZero: true,
+        ticks: {
+          stepSize: 10,
+        },
+      },
+    },
+  },
+});
